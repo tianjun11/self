b6f04b5781b38158e59d6dde1e72a4070df0555b
MYSOFT\tianj02 <tianj02@mingyuanyun.com>
2018-06-23 14:17:25 +0800

    1

---
+++ 单测/WebApplication1/Bussiness/GetUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Bussiness
+{
+    public class GetUser
+    {
+
+        public String GetUseName(string userName)
+        {
+            return "aaa";
+        }
+    }
+}
